ff8088ed733b74b2c343e820adc72d5f910ec5b7
BlackjackVN <cuongtq.dev@gmail.com>
2026-08-03 13:11:04 +0700

    update lại appsetting để dự án vừa chạy local vừa deploy được

--- Blog.API.Tests/CustomWebApplicationFactory.cs
+++ Blog.API.Tests/CustomWebApplicationFactory.cs
@@ -3,6 +3,7 @@ using Blog.Infrastructure.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestPlatform.TestHost;
 using Moq;
@@ -23,6 +24,23 @@ namespace Blog.API.Tests
         }
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
+            // Thiết lập các cấu hình giả lập (dummy settings) dành riêng cho môi trường Test
+            // Việc này giúp tránh bị ghi đè lên các cấu hình SQL Server/Redis thật khi dev ở local
+            builder.ConfigureAppConfiguration((context, config) =>
+            {
+                config.AddInMemoryCollection(new Dictionary<string, string>
+                {
+                    { "ConnectionStrings:DefaultConnection", "Server=localhost;Database=MyBlogTest;Trusted_Connection=True;TrustServerCertificate=True;" },
+                    { "ConnectionStrings:RedisConnection", "localhost:6379" },
+                    { "JWT:SigningKey", "SuperSecretKeyForMyBlogJWT_2026_MustBeAtLeast64CharactersLongForSecurity!!" },
+                    { "JWT:Issuer", "BJVN_Blog" },
+                    { "JWT:Audience", "BJVN_Blog_Client" },
+                    { "CloudinarySettings:CloudName", "test-cloud" },
+                    { "CloudinarySettings:ApiKey", "test-api-key" },
+                    { "CloudinarySettings:ApiSecret", "test-api-secret" }
+                });
+            });
+
             builder.ConfigureServices(services =>
             {
                 // 1. Gỡ bỏ đăng ký AppDbContext thật kết nối với SQL Server
